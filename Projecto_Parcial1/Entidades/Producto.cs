7abd387e3cb07f64c638f0db3ff61592105a43a5
Adriel Villar Vasquez <adriel_villar@ucne.edu.do>
2019-05-29 22:01:56 -0400

    Comenzando con la class entidades

---
+++ Projecto_Parcial1/Entidades/Producto.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Projecto_Parcial1.Entidades
+{
+    class Producto
+    {
+        [Key]
+        int ProductoId;
+        
+
+    }
+}
