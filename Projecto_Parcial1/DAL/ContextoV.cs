6ced6954f8a14c755d2b303a245cb45f6c2edc80
Adriel Villar Vasquez <adriel_villar@ucne.edu.do>
2019-06-02 00:30:23 -0400

    Terminando las clases

--- Projecto_Parcial1/DAL/ContextoV.cs
+++ Projecto_Parcial1/DAL/ContextoV.cs
@@ -1,17 +1,19 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Data.Entity;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using Projecto_Parcial1.Entidades;
 
 namespace Projecto_Parcial1.DAL
 {
-    class ContextoV
+    class ContextoV : DbContext
     {
-        public DbSet<Valor_Inventario> ValorI { get; set; }
+        public DbSet<ValorInventario> ValorInventario { get; set; }
 
 
-        public Contexto() : base("ConStr")
+        public ContextoV() : base("ConStr")
         { }
     }
 }
---
+++ Projecto_Parcial1/Entidades/ValorInventario.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Projecto_Parcial1.Entidades
+{
+   public class ValorInventario
+    {
+        [Key]
+
+        public int Id { get; set; }
+
+        public float Valor_Inventario { get; set; }
+
+        public ValorInventario()
+        {
+            Id = 0;
+            Valor_Inventario = 0.0f;
+        }
+
+
+
+    }
+}
